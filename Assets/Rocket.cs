3ea11e1c55a7a62d17ce8a2f5bbf341b6708bfe1
uduji chikordi <udujichikordi@gmail.com>
2018-10-03 11:38:37 +0100

    intial commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    new Rigidbody rigidbody;
+     AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+     void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // for our thrusting and can thrust while rotating
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+
+            }
+           
+            print("thrusting");
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Rotate(Vector3.forward);
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+       
+        {
+
+            transform.Rotate(-(Vector3.forward));
+            print("Rotating right");
+        
+        }
+    }
+}
