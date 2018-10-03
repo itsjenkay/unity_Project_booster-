d5a126e7af8a500ab1f2447b0cbb6da6ed7d127c
uduji chikordi <udujichikordi@gmail.com>
2018-10-03 14:53:52 +0100

    Using serialzeFiled and working on my rotation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     new Rigidbody rigidbody;
      AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
+
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
@@ -17,13 +21,14 @@ public class Rocket : MonoBehaviour {
 	void Update () {
         Rotate();
         Thrust();
+       
     }
     // thrusting code
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // for our thrusting and can thrust while rotating
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up *mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -40,19 +45,20 @@ public class Rocket : MonoBehaviour {
     //rotation code
     private void Rotate()
     {
-
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         rigidbody.freezeRotation = true;// take manual control of the rotation
         if (Input.GetKey(KeyCode.A))
         {
 
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward*rotationThisFrame);
             print("Rotating left");
         }
         else if (Input.GetKey(KeyCode.D))
 
         {
 
-            transform.Rotate(-(Vector3.forward));
+            transform.Rotate(-(Vector3.forward * rotationThisFrame));
             print("Rotating right");
 
         }
